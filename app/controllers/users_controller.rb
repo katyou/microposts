8767222e5908f1313300574cc484b3c361773d50
Masahiro Ienaga <mainichiaisatu@me.com>
2016-01-08 17:26:35 +0000

    second edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
@@ -19,13 +20,14 @@ class UsersController < ApplicationController
   end
   
   def edit
-    if current_user != (current_user)
-      redirect_to root_path
-    end
+    #if current_user != current_user
+    #  redirect_to root_path
+    #end
     @user = User.find(params[:id])
   end
   
   def update
+    @user = User.find(params[:id])
     if @user = User.new(user_params)
       redirect_to root_path , notice: '更新しました'
     else
