3b6089c8eee217e95f4958a425de844fb7b9cc86
Masahiro Ienaga <mainichiaisatu@me.com>
2016-01-08 14:00:28 +0000

    first commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,45 @@
+class UsersController < ApplicationController
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+  
+  def edit
+    if current_user != (current_user)
+      redirect_to root_path
+    end
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    if @user = User.new(user_params)
+      redirect_to root_path , notice: '更新しました'
+    else
+      render 'edit'
+    end
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+end
+
+
+
