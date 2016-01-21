484bd058b5b075fdd917dfd909b22b2517d59bd3
Masahiro Ienaga <mainichiaisatu@me.com>
2016-01-21 14:08:37 +0000

    favorites

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,5 +30,10 @@ class User < ActiveRecord::Base
     def following?(other_user)
         following_users.include?(other_user)
     end
+    
+    #つぶやき取得のためのメソッド
+    def feed_items
+        Micropost.where(user_id: following_user_ids + [self.id])
+    end
 
 end
